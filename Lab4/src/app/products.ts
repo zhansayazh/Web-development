cdf3d98a8b1f8dfd7983b2cc0cf4874c06201026
zhansayazh <78623792+zhansayazh@users.noreply.github.com>
2021-02-24 13:54:47 +0500

    Add files via upload

---
+++ Lab4/src/app/products.ts
@@ -0,0 +1,89 @@
+export const products = [
+    {
+      id: 1,
+      name: 'HP 67XL | Ink Cartridge | Black | 3YM57AN',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71lle5IWbTL._AC_SL1500_.jpg',
+      description: 'HP 67XL ink cartridges work with: HP ENVY 6052, 6055, 6058, 6075, HP ENVY Pro 6452, 6455, 6458. HP DeskJet 1255, 2732, 2752, 2755, DeskJet Plus 4140, 4152, 4155, 4158. Save up to 25% with high-yield HP ink. HP 67 ink cartidge yield (approx.) per cartridge: 240 pages. Up to 2x more prints with Original HP ink vs refill cartridges',
+      rating: '5/5',
+      link: 'https://www.amazon.com/HP-Cartridge-Black-3YM57AN-Tri-Color/dp/B08412PTS8/ref=sr_1_6?dchild=1&fst=as%3Aoff&pf_rd_i=16225007011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=74069509-93ef-4a3c-8dca-a9e3fa773a64&pf_rd_r=K8CR0X6TAZJ8N9ZA29JT&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1487012920&rnid=16225007011&s=computers-intl-ship&sr=1-6'
+    },
+    {
+      id: 2,
+      name: 'HP VH240a 23.8-Inch Full HD',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71trhuzbhML._AC_SL1500_.jpg',
+      description: '1080p IPS LED Monitor with Built-In Speakers and VESA Mounting, Rotating Portrait & Landscape, Tilt, and HDMI & VGA Ports (1KL30AA) - Black. 23.8-inch Full HD monitor (1920 x 1080p @ 60 Hz) with 16:9 aspect ratio and an anti-glare matte IPS LED-backlit panel (2 million pixels, 16.7 million colors)',
+      rating: '4.7/5',
+      link: 'https://www.amazon.com/HP-23-8-inch-Adjustment-Speakers-VH240a/dp/B072M34RQC/ref=lp_16225007011_1_11'
+    },
+    {
+      id: 3,
+      name: 'Acer Aspire 5 Slim Laptop',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71vvXGmdKWL._AC_SL1500_.jpg',
+      description: 'AMD Ryzen 3 3200U Dual Core Processor (Up to 3.5GHz); 4GB DDR4 Memory; 128GB PCIe NVMe SSD. 15.6 inches full HD (1920 x 1080) widescreen LED backlit IPS display; AMD Radeon Vega 3 Mobile Graphics',
+      rating: '4.6/5',
+      link: 'https://www.amazon.com/Acer-Display-Graphics-Keyboard-A515-43-R19L/dp/B07RF1XD36/ref=lp_16225007011_1_6'
+    },
+    {
+      id: 4,
+      name: 'Acer SB220Q bi 21.5 Inches Full HD',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg',
+      description: '21.5 inches Full HD (1920 x 1080) widescreen IPS display. And Radeon free sync technology. No compatibility for VESA Mount. Refresh rate: 75 hertz - Using HDMI port. Zero-frame design | ultra-thin | 4ms response time | IPS panel',
+      rating: '4.7/5',
+      link: 'https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=lp_16225007011_1_5'
+    },
+    {
+      id: 5,
+      name: 'NETGEAR 5-Port Gigabit Ethernet Unmanaged Switch (GS305)',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/814lMQ5y9GL._AC_SL1500_.jpg',
+      description: 'Plug and play: Simple setup with no software to install or configuration needed. ',
+      rating: '4.8/5',
+      link: 'https://www.amazon.com/NETGEAR-5-Port-Gigabit-Ethernet-Unmanaged/dp/B07S98YLHM/ref=sr_1_6?dchild=1&fst=as%3Aoff&pf_rd_i=16225007011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=74069509-93ef-4a3c-8dca-a9e3fa773a64&pf_rd_r=9CEY8CN88KAW3C7T75A2&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1487012920&rnid=16225007011&s=computers-intl-ship&sr=1-6'
+    }, 
+    {
+      id: 6,
+      name: 'HyperX Cloud II - Gaming Headset',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/719KtJ-RAFL._AC_SL1417_.jpg',
+      description: 'Designed for comfort – Exceptionally comfortable memory foam ear cushions and padded leatherette headband help keep you focused on gaming. Supreme audio quality – Large 53 millimeter drivers provide high-quality audio. Hear in-game details better and get the in-game advantage on your opponents. 7.1 virtual surround sound – Truly immerse yourself in your games with hardware-driven virtual 7.1 surround sound for precisely located audio. Passive noise cancellation – The closed ear cup design blocks out outside distractions to keep you in the game',
+      rating: '4.6/5',
+      link: 'https://www.amazon.com/HyperX-Cloud-Gaming-Headset-KHX-HSCP-GM/dp/B00SAYCVTQ/ref=sxin_10_ac_d_pm?ac_md=3-2-QWJvdmUgJDUw-ac_d_pm&cv_ct_cx=video+games&dchild=1&keywords=video+games&pd_rd_i=B00SAYCVTQ&pd_rd_r=dcf918f6-4f04-4e74-9039-73f3d972a82b&pd_rd_w=hvSI5&pd_rd_wg=BmUBa&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=5HARABZRQW6YVWFN74QD&pf_rd_s=merchandised-search-6&pf_rd_t=101&psc=1&qid=1614092615&sr=1-2-22d05c05-1231-4126-b7c4-3e7a9c0027d0'
+    }, 
+    {
+      id: 7,
+      name: 'Sorry! Game',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/811mGsv%2BY8L._AC_SL1500_.jpg',
+      description: 'Ditch the TV, and re ignite family night with the get together amusement of a Hasbro game. Party it up, and surprise guests at your next event with laugh out loud game from Hasbro gaming. Nostalgic tabletop gameplay meets interactive digital content for an immersive gaming experience',
+      rating: '4.8/5',
+      link: 'https://www.amazon.com/Hasbro-Gaming-A5065-Sorry-Game/dp/B076HK9H7Z/ref=sxin_10_ac_d_pm?ac_md=1-0-VW5kZXIgJDEw-ac_d_pm&cv_ct_cx=board+games&dchild=1&keywords=board+games&pd_rd_i=B076HK9H7Z&pd_rd_r=b4194cb4-4135-4b1f-8abc-23e6334c047a&pd_rd_w=XXhoQ&pd_rd_wg=J3WVy&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=5HARABZRQW6YVWFN74QD&pf_rd_s=merchandised-search-6&pf_rd_t=101&psc=1&qid=1614092918&sr=1-1-22d05c05-1231-4126-b7c4-3e7a9c0027d0'
+    },
+    {
+      id: 8,
+      name: 'Lenovo Tab M10 Plus',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71sfODG%2Bc%2BL._AC_SL1500_.jpg',
+      description: 'Premium look & feel with metal back cover and slim, narrow bezels. Enjoy your favorite videos on the 10.3" FHD display with TDDI technology. Fast and powerful Octa-Core processor with up to 2.3 GHz main frequency for quick performance. Never miss a moment with 8 MP rear + 5 MP front cameras, dual microphones, and 2 side speakers tuned with Dopoundsy Atmos',
+      rating: '4.5/5',
+      link: 'https://www.amazon.com/Lenovo-Android-Octa-Core-Processor-ZA5T0300US/dp/B083JXPDL5/ref=sr_1_4?dchild=1&keywords=tablets&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=5HARABZRQW6YVWFN74QD&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093185&sr=8-4'
+    },
+    {
+      id: 9,
+      name: 'Standio Car Phone Holder Mount',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81vqCMbcJ%2BL._AC_SL1500_.jpg',
+      description: 'Extremely Powerful Suction: Through a process of experimentation and innovation, we used top-of-the-line silicone to create a seriously strong suction cup that’s 4x stronger than previous models. Maximum Compatibility: Truly universal phone holder provides a perfectly secure hold for all cell phones including iPhone 12 Series/ 11 Series/ X Series, Samsung Galaxy 20/10 Series/Note 20/10 Series, Google Pixel and other smartphones.',
+      rating: '4.9/5',
+      link: 'https://www.amazon.com/Standio-Universal-Suction-Windshield-Compatible/dp/B08NV7K5HK/ref=sr_1_9_sspa?dchild=1&keywords=mobile+phones&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=67c6cf47-c067-447f-a799-b2cb009a7a6e&pf_rd_r=5HARABZRQW6YVWFN74QD&pf_rd_s=merchandised-search-8&pf_rd_t=101&qid=1614093337&sr=8-9-spons&psc=1&smid=A2Z53E0XBTHXKI&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFQTjU1MjYyUlhZOEcmZW5jcnlwdGVkSWQ9QTAxMjI1MzAxRlVZQ1g3NUdTSk03JmVuY3J5cHRlZEFkSWQ9QTAwMzE5NTEySk04QUU3R1FWTERRJndpZGdldE5hbWU9c3BfbXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+    },
+    {
+      id: 10,
+      name: 'FirYawee Smartwatch for Android Phones and iOS Phones',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61MVEgKpAzL._AC_SL1500_.jpg',
+      description: '【New Upgraded Bright & Clear Display】 Equipped with 1.4 inch full touch screen and TFT-LCD clear display,this New upgraded Smart watch personalized interactive interfacecan easily catch your eyes and follow your heart.Strong and durable 2.5D glass makes your smart watch much more scratch-resistant. It is easy to operate and is smooth and responsive.',
+      rating: '4.2/5',
+      link: 'https://www.amazon.com/FirYawee-Smartwatch-Android-Waterproof-Distance/dp/B08F54L2FP/ref=sr_1_5?dchild=1&keywords=smart+watch&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=67c6cf47-c067-447f-a799-b2cb009a7a6e&pf_rd_r=5HARABZRQW6YVWFN74QD&pf_rd_s=merchandised-search-8&pf_rd_t=101&qid=1614093808&sr=8-5'
+    }
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at https://angular.io/license
+  */
