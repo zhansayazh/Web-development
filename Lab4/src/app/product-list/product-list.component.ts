dc8d50b3301c7c2cb8c63f8050ebde933186369a
zhansayazh <78623792+zhansayazh@users.noreply.github.com>
2021-02-24 13:57:19 +0500

    Add files via upload

---
+++ Lab4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share(link:string):void {
+    window.open("https://t.me/share/url?url=Zhansaya Store&text="+link);
+  }
+  
+}
