35b0254970d4bae2a726facdbe4bd983482b7310
zhansayazh <78623792+zhansayazh@users.noreply.github.com>
2021-03-31 00:50:38 +0600

    Add files via upload

---
+++ Lab7/Task1/while/a.py
@@ -0,0 +1,4 @@
+b = int(input())
+for i in range(1, b+1):
+    if(i * i <= b):
+        print(i*i)
---
+++ Lab7/Task1/while/d.py
@@ -0,0 +1,9 @@
+n = int(input())
+i = 1
+while i<=n:
+    if i==n:
+        print("YES")
+        break
+    i*=2
+else:
+    print("NO")
